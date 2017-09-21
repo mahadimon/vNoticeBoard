be1d440c74b526f8f900750a48fdbd13d3900c0d
mahadimon101 <mahadimon101@gmail.com>
2017-09-21 19:41:22 +0600

    third version

---
+++ src/main/java/com/livinoncodes/vnoticeboard/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.livinoncodes.vnoticeboard.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BoardController.class);
+	
+	@RequestMapping(value="/{boardName}", method=RequestMethod.GET)
+	public String showBoard(@PathVariable("boardName") String boardName, Model model){
+		LOGGER.debug("Rendering board page: "+boardName);
+		
+		model.addAttribute("boardName", boardName);
+		
+		return "board";
+	}
+}
